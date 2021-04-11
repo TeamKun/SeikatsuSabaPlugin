0e84e4d7f648bf36e0738f6d7f15107174accb58
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-04-11 16:05:59 +0900

    :bug: fix: Mobのダメージ(壁)

--- src/main/java/net/kunmc/lab/seikatsusabaplugin/events/HatEventListener.java
+++ src/main/java/net/kunmc/lab/seikatsusabaplugin/events/HatEventListener.java
@@ -5,16 +5,17 @@ import org.bukkit.entity.Boat;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class HatEventListener implements Listener
 {
     @EventHandler
-    public void onDamageEvent(EntityDamageByBlockEvent e)
+    public void onDamageEvent(EntityDamageEvent e)
     {
-        if (e.getEntity().getVehicle() != null && !(e.getEntity() instanceof Minecart || e.getEntity() instanceof Boat))
+        if(e.getCause() == EntityDamageEvent.DamageCause.FLY_INTO_WALL)
+        if (e.getEntity().getVehicle() != null && !(e.getEntity().getVehicle() instanceof Minecart || e.getEntity().getVehicle() instanceof Boat))
             e.setCancelled(true);
     }
 

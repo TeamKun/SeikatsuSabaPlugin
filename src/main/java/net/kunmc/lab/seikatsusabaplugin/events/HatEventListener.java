80ace192c52b9de046b919e26e5af8a827d7782e
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-04-11 15:52:06 +0900

    :bug: fix: Mobのダメージ

--- src/main/java/net/kunmc/lab/seikatsusabaplugin/events/HatEventListener.java
+++ src/main/java/net/kunmc/lab/seikatsusabaplugin/events/HatEventListener.java
@@ -1,13 +1,23 @@
 package net.kunmc.lab.seikatsusabaplugin.events;
 
 import net.kunmc.lab.seikatsusabaplugin.commands.CommandUnHat;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class HatEventListener implements Listener
 {
+    @EventHandler
+    public void onDamageEvent(EntityDamageByBlockEvent e)
+    {
+        if (e.getEntity().getVehicle() != null && !(e.getEntity() instanceof Minecart || e.getEntity() instanceof Boat))
+            e.setCancelled(true);
+    }
+
     @EventHandler
     public void onLeave(PlayerQuitEvent e)
     {

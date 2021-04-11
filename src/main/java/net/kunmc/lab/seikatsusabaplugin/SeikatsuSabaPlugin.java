69d71536b52a2172ea0af88283e81cada8a5430c
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-04-11 10:24:27 +0900

    :art: refactor: 整形

--- src/main/java/net/kunmc/lab/seikatsusabaplugin/SeikatsuSabaPlugin.java
+++ src/main/java/net/kunmc/lab/seikatsusabaplugin/SeikatsuSabaPlugin.java
@@ -6,7 +6,6 @@ import net.kunmc.lab.seikatsusabaplugin.commands.CommandUnHat;
 import net.kunmc.lab.seikatsusabaplugin.events.HatEventListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 
 public final class SeikatsuSabaPlugin extends JavaPlugin
 {
